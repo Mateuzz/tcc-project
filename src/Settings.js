b43fdbea741f723d1e3199786496e5e52f7d4b45
Mateus Freitas <vactusorigin@gmail.com>
2023-10-31 00:04:55 -0300

    Server.php com tipo corrigido

--- src/Settings.js
+++ src/Settings.js
@@ -3,6 +3,6 @@ import Clock from "./Clock.js";
 window.defaultClock = new Clock();
 window.DEBUG_MODE = 0;
 window.PREFIX_PATH = "models/";
-window.BROWSER_NAME = "firefox";
+window.BROWSER_NAME = "edge";
 defaultClock.begin("startupTime");
 
